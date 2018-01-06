221aebff28e0c8893d4555a328a2899d9be7b6e0
Arun Menon <ntarunmenon@gmail.com>
2018-01-06 15:36:30 +0400

    Week 1 - Suffix Tree Fixes

--- Algorithm_On_Strings/src/week1/practice/SuffixTree.java
+++ Algorithm_On_Strings/src/week1/practice/SuffixTree.java
@@ -9,55 +9,65 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-
-
- 
 class SuffixNode {
 	public int endIndex;
 	public String edgeLabel;
 	public boolean visited;
-	
+
 	public SuffixNode(int endNode, String path) {
 		super();
 		this.endIndex = endNode;
 		this.edgeLabel = path;
 		visited = false;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		SuffixNode other = (SuffixNode) obj;
+
+		if (other != null && other.endIndex == this.endIndex && other.edgeLabel != null
+				&& other.edgeLabel.equals(this.edgeLabel)) {
+			return true;
+		}
+
+		return false;
+	}
+
 	@Override
 	public String toString() {
-		return "endIndex=" + endIndex + "   edgeLabel=" +  edgeLabel;
+		return "endIndex=" + endIndex + "   edgeLabel=" + edgeLabel;
 	}
 }
 
-
 public class SuffixTree {
+	List<String> paths = new ArrayList<String>();
 	int edgeIndex = 0;
 	public Map<Integer, List<SuffixNode>> adj = new HashMap<>();
-	
-    class FastScanner {
-        StringTokenizer tok = new StringTokenizer("");
-        BufferedReader in;
+	private boolean[] visited;
 
-        FastScanner() {
-            in = new BufferedReader(new InputStreamReader(System.in));
-        }
+	class FastScanner {
+		StringTokenizer tok = new StringTokenizer("");
+		BufferedReader in;
 
-        String next() throws IOException {
-            while (!tok.hasMoreElements())
-                tok = new StringTokenizer(in.readLine());
-            return tok.nextToken();
-        }
+		FastScanner() {
+			in = new BufferedReader(new InputStreamReader(System.in));
+		}
 
-        int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
-    }
-    
-    // Build a suffix tree of the string text and return a list
-    // with all of the labels of its edges (the corresponding 
-    // substrings of the text) in any order.
-    public List<String> computeSuffixTreeEdges(String text) {
+		String next() throws IOException {
+			while (!tok.hasMoreElements())
+				tok = new StringTokenizer(in.readLine());
+			return tok.nextToken();
+		}
+
+		int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		}
+	}
+
+	// Build a suffix tree of the string text and return a list
+	// with all of the labels of its edges (the corresponding
+	// substrings of the text) in any order.
+	public List<String> computeSuffixTreeEdges(String text) {
 		for (int index = text.length() - 1; index >= 0; index--) {
 			String currentSuffixString = text.substring(index, text.length());
 			int currentIndexinGraph = 0;
@@ -67,35 +77,38 @@ public class SuffixTree {
 				List<SuffixNode> adjList = adj.get(currentIndexinGraph);
 				if (adjList != null) {
 					int matchNodeIndex = -1;
-	            
-					for(int nodeIndex = 0;nodeIndex < adjList.size(); nodeIndex ++){
-		        		SuffixNode node = adjList.get(nodeIndex);
-						if(node.edgeLabel.charAt(0) == currentSuffixString.charAt(0)){
+
+					for (int nodeIndex = 0; nodeIndex < adjList.size(); nodeIndex++) {
+						SuffixNode node = adjList.get(nodeIndex);
+						if (node.edgeLabel.charAt(0) == currentSuffixString.charAt(0)) {
 							matchNodeIndex = nodeIndex;
 							break;
 						}
-	            	}
-					if(matchNodeIndex != -1){
-						SuffixNode node = adjList.get(matchNodeIndex);	
-						//If the length of the node in graph is 1
-							if(node.edgeLabel.length() ==1){
-								currentSuffixString = currentSuffixString.substring(1);
-								currentIndexinGraph = node.endIndex;
-							}else{
-								int noOfCommonMatches = findNoOfCommonMatches(node.edgeLabel,currentSuffixString);
-								String currentNode = node.edgeLabel;
-								node.edgeLabel = currentNode.substring(0, noOfCommonMatches);
-								currentNode = currentNode.substring(noOfCommonMatches,currentNode.length());
-								currentSuffixString = currentSuffixString.substring(noOfCommonMatches,currentSuffixString.length());
+					}
+					if (matchNodeIndex != -1) {
+						SuffixNode node = adjList.get(matchNodeIndex);
+						// If the length of the node in graph is 1
+						if (node.edgeLabel.length() == 1) {
+							currentSuffixString = currentSuffixString.substring(1);
+							currentIndexinGraph = node.endIndex;
+						} else {
+							int noOfCommonMatches = findNoOfCommonMatches(node.edgeLabel, currentSuffixString);
+							String currentNode = node.edgeLabel;
+							node.edgeLabel = currentNode.substring(0, noOfCommonMatches);
+							if(!(noOfCommonMatches == currentNode.length())){
+								currentNode = currentNode.substring(noOfCommonMatches, currentNode.length());
 								currentIndexinGraph = addnewNode(currentNode, node.endIndex);
-								currentIndexinGraph = addnewNode(currentSuffixString, node.endIndex);
-								stopCurrentSuffixIteration = true;
 							}
-					}else{
+							currentSuffixString = currentSuffixString.substring(noOfCommonMatches,
+									currentSuffixString.length());
+							currentIndexinGraph = addnewNode(currentSuffixString, node.endIndex);
+							stopCurrentSuffixIteration = true;
+						}
+					} else {
 						currentIndexinGraph = addnewNode(currentSuffixString, currentIndexinGraph);
 						stopCurrentSuffixIteration = true;
 					}
-					
+
 				} else {
 					currentIndexinGraph = addnewNode(currentSuffixString, currentIndexinGraph);
 					stopCurrentSuffixIteration = true;
@@ -112,11 +125,43 @@ public class SuffixTree {
 				}
 			}
 		}
-
+		System.out.println();
+		visited = new boolean[edgeIndex + 1];
+		paths.add("");
+		explore(0);
 		return result;
 	}
 
-    private int findNoOfCommonMatches(String edgeLabel, String currentSuffixString) {
+	private void explore(Integer startNode) {
+		visited[startNode] = true;
+		
+		if(!adj.containsKey(startNode)){
+			printPath();
+			paths.remove(paths.size() - 1);
+			return;
+		}
+		
+		
+		List<SuffixNode> nodes = adj.get(startNode);
+		for (SuffixNode neighbour : nodes) {
+			if (!visited[neighbour.endIndex]) {
+				neighbour.visited = true;
+				paths.add(neighbour.edgeLabel);
+				explore(neighbour.endIndex);
+			}
+		}
+		paths.remove(paths.size() - 1);
+	}
+
+	private void printPath() {
+		String path = "";
+		for (String node : paths) {
+			path = path + node;
+		}
+		System.out.println(path);
+	}
+
+	private int findNoOfCommonMatches(String edgeLabel, String currentSuffixString) {
 		int index = 0;
 		while (index < edgeLabel.length() && edgeLabel.charAt(index) == currentSuffixString.charAt(index)) {
 			index++;
@@ -127,31 +172,30 @@ public class SuffixTree {
 	private int addnewNode(String currentSuffixString, int currentIndexinGraph) {
 		edgeIndex++;
 		SuffixNode newNode = new SuffixNode(edgeIndex, currentSuffixString);
-		if(adj.get(currentIndexinGraph) != null){
+		if (adj.get(currentIndexinGraph) != null) {
 			adj.get(currentIndexinGraph).add(newNode);
-		}else{
+		} else {
 			List<SuffixNode> node = new ArrayList<>();
 			node.add(newNode);
 			adj.put(currentIndexinGraph, node);
 		}
 		return edgeIndex;
 	}
-	
-	
-    static public void main(String[] args) throws IOException {
-        new SuffixTree().run();
-    }
 
-    public void print(List<String> x) {
-        for (String a : x) {
-            System.out.println(a);
-        }
-    }
+	static public void main(String[] args) throws IOException {
+		new SuffixTree().run();
+	}
 
-    public void run() throws IOException {
-        FastScanner scanner = new FastScanner();
-        String text = scanner.next();
-        List<String> edges = computeSuffixTreeEdges(text);
-        print(edges);
-    }
+	public void print(List<String> x) {
+		for (String a : x) {
+			//System.out.println(a);
+		}
+	}
+
+	public void run() throws IOException {
+		FastScanner scanner = new FastScanner();
+		String text = scanner.next();
+		List<String> edges = computeSuffixTreeEdges(text);
+		print(edges);
+	}
 }
